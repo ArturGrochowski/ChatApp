878d055b5983c761e89f4478938e65c7e950fe1f
ArturGrochowski <44144541+ArturGrochowski@users.noreply.github.com>
2019-03-15 15:35:49 +0100

    (Shadownig) ChatApp

---
+++ app/src/main/java/com/gmail/arturgrochowski/User/UserListAdapter.java
@@ -0,0 +1,70 @@
+package com.gmail.arturgrochowski.User;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.gmail.arturgrochowski.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserListViewHolder> {
+
+    ArrayList<UserObject> userList;
+
+    public UserListAdapter(ArrayList<UserObject> userList) {
+        this.userList = userList;
+
+    }
+
+    @NonNull
+    @Override
+    public UserListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
+
+        UserListViewHolder rcv = new UserListViewHolder(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserListViewHolder holder, int position) {
+        holder.mName.setText(userList.get(position).getName());
+        holder.mPhone.setText(userList.get(position).getPhone());
+
+        holder.mLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key  = FirebaseDatabase.getInstance().getReference().child("chat").push().getKey();
+
+                FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("chat").child(key).setValue(true);
+                FirebaseDatabase.getInstance().getReference().child("user").child(userList.get(position).getUid()).child("chat").child(key).setValue(true);
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class UserListViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mName, mPhone;
+        public LinearLayout mLayout;
+        public UserListViewHolder(View itemView) {
+            super(itemView);
+            mName = itemView.findViewById(R.id.name);
+            mPhone = itemView.findViewById(R.id.phone);
+            mLayout = itemView.findViewById(R.id.layout);
+        }
+    }
+}
